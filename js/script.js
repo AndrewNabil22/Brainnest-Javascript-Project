c6beea7a40a47c0d2ab04032c53168995225f58f
AndrewNabil22 <andrewnabil41@gmail.com>
2023-05-06 17:22:13 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
-let firstMsg = "Let's play Rock Paper Scissors.";
+let welcomeMsg = "Let's play Rock Paper Scissors.";
 let startMsg = "Are you ready? Write 'yes' if you are.";
   
-  alert(firstMsg);
+  alert(welcomeMsg);
 
 let answer = prompt(startMsg);
 
@@ -42,73 +41,9 @@ let validRounds = 1;
   
     if (validRounds === 6) {
       break;
-=======
-// Declaring available choices for the game
-const choices = ["rock", "paper", "scissors"];
-
-// Function that randomly selects a choice for the computer
-const computerPlay = () => choices[Math.floor(Math.random() * choices.length)];
-
-// Function that plays one round of the game between the user and the computer
-const playRound = (playerSelection, computerSelection) => {
-  playerSelection = playerSelection.toLowerCase();
-  // Check if the player's choice is valid
-  if (!choices.includes(playerSelection)) {
-    alert(`Invalid choice: ${playerSelection}.`);
-  }
-  // Determine the winner of the round and return the result as a string
-  if (playerSelection === computerSelection) {
-    return "It's a tie!";
-  } else if (
-    (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") ||
-    (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") ||
-    (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper")
-  ) {
-    return `You win! ${playerSelection} beats ${computerSelection}.`;
-  } else {
-    return `You lose! ${computerSelection} beats ${playerSelection}.`;
-  }
-};
-
-// Function that runs the game for 5 rounds
-const game = () => {
-  //These two variables must be incremented after each round
-  let playerScore = 0;
-  let computerScore = 0;
-  // Play 5 rounds
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = "";
-    // Ask the user to enter a valid choice for each round
-    while (!choices.includes(playerSelection)) {
-      playerSelection = prompt(`Round ${i+1}: Enter your choice: ${choices.join(", ")}`).toLowerCase();
-      try {
-        // Play one round with the user's choice and a random choice for the computer
-        playRound(playerSelection, computerPlay());
-      } catch (e) {
-        alert(e.message);
-      }
-    }
-    // Play one round with the user's choice and a random choice for the computer
-    const computerSelection = computerPlay();
-      if (playerSelection) {
-      try {
-        // Get the result of the round and update the score
-        const result = playRound(playerSelection, computerSelection);
-        console.log(result);
-
-    if (result.startsWith("You win!")) {
-          playerScore++;
-        } else if (result.startsWith("You lose!")) {
-          computerScore++;
-        }
-  } catch (e) {
-        alert(e.message);
-      }
->>>>>>> 2df7a3dd589aace8a1aad5692744db1a7f275b29
     }
   }
 
-<<<<<<< HEAD
   function playerSelection() {
     let playerChoice = prompt("Round " + validRounds + ": " + "Type 'rock $ paper $ scissors, shoot!'");
   
@@ -160,28 +95,9 @@ function game(playerSelection, computerSelection) {
     computerPoints++;
   } else if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
     result = "It's a tie";
-
-if (playerPoints > computerPoints) {
-  console.log("Congratulations, You Win!");
-} else if (playerPoints < computerPoints) {
-  console.log("Bad luck, Computer Wins!");
-} else if (playerPoints === computerPoints) {
-  console.log("Hard game, good luck");
-} else {
-  console.log("Something went wrong, start over.");
-}
-
-
-  // Determine the winner of the game based on the final score
-  if (playerScore > computerScore) {
-    console.log(`You win the game! Final score: ${playerScore}-${computerScore}`);
-  } else if (computerScore > playerScore) {
-    console.log(`You lose the game! Final score: ${playerScore}-${computerScore}`);
->>>>>>> 2df7a3dd589aace8a1aad5692744db1a7f275b29
   } else {
-    console.log(`It's a tie! Final score: ${playerScore}-${computerScore}`);
+    result = "Invalid input. Please choose 'rock', 'paper', or 'scissors'.";
   }
-<<<<<<< HEAD
 
   return result;
 }
@@ -202,8 +118,3 @@ function winner(){
 }
 
 winner();
-=======
-};
-
-game()
->>>>>>> 2df7a3dd589aace8a1aad5692744db1a7f275b29
